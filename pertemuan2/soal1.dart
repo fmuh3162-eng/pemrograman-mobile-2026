5fe8c08546ba37de96408b8fce970584c5493ffe
MUH. FARID <muhf76671@gmail.com>
2026-02-28 16:38:06 +0800

    Menambahkan tugas pertemuan 2

---
+++ pertemuan2/soal1.dart
@@ -0,0 +1,5 @@
+void  main () {
+    for (int i = 0; i < 10; i++) {
+        print ('nama saya adalah fulan, sekarang berumur ${18 - i}');
+    }
+}
---
+++ pertemuan2/soal4.dart
@@ -0,0 +1,7 @@
+void main() {
+  print("Nama Lengkap      : Muh Farid");
+  print("NIM               : H0724511");
+  print("IPK               : 3.71");
+  print("Semester Saat Ini : 4");
+  print("Status Beasiswa   : tiddak meneriman beasiswa");
+}
